2cf1a3d82e6eed20be469682f8df6a5cf850a9f0
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2022-10-10 10:49:08 +0300

    Adding more shit

---
+++ BetterBookList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterBookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
