e54944ee54a90a922d3f98aeb7d6cdd22692fd98
roooff <114150832+roooff@users.noreply.github.com>
2022-10-31 15:49:03 +0200

    EBETE SI MAIKATA

---
+++ ПГПИ/Протокол№4/Employee Managment/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using static System.Windows.Forms.AxHost;
+
+namespace Employee_Managment
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Zip{ get; set; }
+
+        public string Email{ get; set; }
+
+        public string Phone{ get; set; }
+    }
+   public string GetAddress()
+    {
+        this.GetAddress =
+        return FirstName + ""+LastName +" "+Environment.NewLine+Street + Environment.NewLine + City +" "
+        +State +" "+Zip;
+    }
+   
+}
