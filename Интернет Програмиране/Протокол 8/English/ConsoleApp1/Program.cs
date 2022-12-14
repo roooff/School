8bc19dd663beaee852c724e8030b14d672516f81
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2022-12-14 17:59:18 +0200

    Protocol n8

---
+++ Интернет Програмиране/Протокол 8/English/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int maxCount = 0;
+            int mostFrequentNum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int count = numbers.Length;
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    mostFrequentNum = numbers[i];
+                }
+            }
+            Console.WriteLine(mostFrequentNum);
+        }
+    }
+}
---
+++ Интернет Програмиране/Протокол 8/English/Spisuk_sus_uchenici/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Spisuk_sus_uchenici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdAdd = new System.Windows.Forms.Button();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(16, 39);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(100, 20);
+            this.textInput.TabIndex = 1;
+            this.textInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textInput_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Въведете запис за добавяне";
+            // 
+            // cmdAdd
+            // 
+            this.cmdAdd.Location = new System.Drawing.Point(266, 59);
+            this.cmdAdd.Name = "cmdAdd";
+            this.cmdAdd.Size = new System.Drawing.Size(75, 23);
+            this.cmdAdd.TabIndex = 3;
+            this.cmdAdd.Text = "Добавяне";
+            this.cmdAdd.UseVisualStyleBackColor = true;
+            this.cmdAdd.Click += new System.EventHandler(this.cmdAdd_Click);
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.Location = new System.Drawing.Point(266, 88);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(75, 23);
+            this.cmdDelete.TabIndex = 4;
+            this.cmdDelete.Text = "Изтриване";
+            this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(266, 117);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(75, 23);
+            this.cmdClear.TabIndex = 5;
+            this.cmdClear.Text = "Изчисти";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(266, 146);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(75, 23);
+            this.cmdExit.TabIndex = 6;
+            this.cmdExit.Text = "Изход";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 274);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.cmdAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Списъчна кутия";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdAdd;
+        private System.Windows.Forms.Button cmdDelete;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.Button cmdExit;
+    }
+}
+
